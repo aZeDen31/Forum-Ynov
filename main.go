ac19ee44bbf4ec266c4d9157f024cbf685ea4610
dmateo <mateo.defief@ynov.com>
2025-03-10 09:35:51 +0100

    [+]Changement chemin redirection

--- main.go
+++ main.go
@@ -6,75 +6,75 @@ import (
 	"net/http"
 )
 
-
-
 func main() {
 	server()
 }
 
-func server(){
-	fileServer := http.FileServer(http.Dir("./html"))
+func server() {
+	fileServer := http.FileServer(http.Dir("./HTML"))
 
-	fs := http.FileServer(http.Dir("./styles"))
-	http.Handle("/assets/", http.StripPrefix("/styles/", fs)) 
+	fs := http.FileServer(http.Dir("./CSS"))
+	http.Handle("/CSS/", http.StripPrefix("/CSS/", fs))
+
+	fd := http.FileServer(http.Dir("./img"))
+	http.Handle("/img/", http.StripPrefix("/img/", fd))
 
 	http.Handle("/", fileServer)
 	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/register", registerHandler) 
+	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/profile", profileHandler)
 
-	fmt.Println("clique sur le lien http://localhost:7000/")
-	if err := http.ListenAndServe(":7000", nil); err != nil {
+	fmt.Println("clique sur le lien http://localhost:5500/")
+	if err := http.ListenAndServe(":5500", nil); err != nil {
 		panic(err)
 	}
 
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request){
-	tmpl := template.Must(template.ParseFiles("html/login.html"))
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("HTML/connexion.html"))
 	tmpl.Execute(w, nil)
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request){
-	tmpl := template.Must(template.ParseFiles("html/register.html"))
-
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("HTML/inscription.html"))
 	tmpl.Execute(w, nil)
 }
 
-func profileHandler(w http.ResponseWriter, r *http.Request){
-	tmpl := template.Must(template.ParseFiles("html/profile.html"))
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("HTML/profile.html"))
 	tmpl.Execute(w, nil)
-} 
+}
 
-func Signup (w http.ResponseWriter, r *http.Request){
+func Signup(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
 
-		// faut insérer ici la query vers la db 
+		// faut insérer ici la query vers la db
 		// penser a check si user existe déjà
 		//si non l'add a la db
 		fmt.Printf("Username: %s, Password: %s\n", username, password)
 		if true {
 			http.Redirect(w, r, "/", http.StatusSeeOther)
 			return
-		}else{
-			fmt.Fprintf(w,"Error user is already registered")
+		} else {
+			fmt.Fprintf(w, "Error user is already registered")
 		}
 	}
 
-	tmpl, err := template.ParseFiles("html/register")
+	tmpl, err := template.ParseFiles("html/inscription.html")
 	if err != nil {
-		 http.Error(w, err.Error(), http.StatusInternalServerError)
-		  return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	tmpl.Execute(w, nil)
 
 }
 
-func LoginPage(w http.ResponseWriter, r *http.Request){
+func LoginPage(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == "POST"{
+	if r.Method == "POST" {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
 
@@ -82,15 +82,15 @@ func LoginPage(w http.ResponseWriter, r *http.Request){
 		fmt.Printf("Username: %s, Password: %s\n", username, password)
 		if true {
 			http.Redirect(w, r, "/profile", http.StatusSeeOther)
-			return 
-		}else{
-			fmt.Fprintf(w,"Error user is not registered")
+			return
+		} else {
+			fmt.Fprintf(w, "Error user is not registered")
 		}
 	}
-	tmpl, err := template.ParseFiles("templates/login.html")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    tmpl.Execute(w, nil)
+	tmpl, err := template.ParseFiles("HTML/connexion.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
